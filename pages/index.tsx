d9bd3540e11b9ed61728f9ceb276587ce9ff238b
Norbertas Murauskas <nmurauskasdarbo@gmail.com>
2021-07-20 16:25:07 +0200

    uppercase

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 import Head   from 'next/head'
 
-import Footer from '../components/combined/footer/footer'
+import Footer from '../components/combined/footer/Footer'
 import Header from '../components/combined/header/Header'
-import Main   from '../components/combined/main/main'
+import Main   from '../components/combined/main/Main'
 import styles from '../styles/Home.module.scss'
 
 
