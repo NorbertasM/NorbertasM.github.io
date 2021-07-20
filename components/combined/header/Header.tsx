45e21602299676626eb64738429e967258002dd7
Norbertas Murauskas <nmurauskasdarbo@gmail.com>
2021-07-20 17:13:25 +0200

    header test 4

--- components/combined/header/Header.tsx
+++ components/combined/header/Header.tsx
@@ -1,18 +1,18 @@
-// import HeaderName from '../../ui components/header-name/header-name';
-// import Toggle     from '../../ui components/toggle/toggle';
+import HeaderName from '../../ui components/header-name/header-name';
+import Toggle     from '../../ui components/toggle/toggle';
 
-// import styles     from './header.module.scss'
+import styles     from './header.module.scss'
 
 
-const Header = () => {
-  return (<p>fak</p>
-    // <div className={styles.titleContainer}>
-      //  <HeaderName />
-      //  <div className={styles.toggle}>
-        // <Toggle />
-      // </div>
-    // </div>
+const MainHeader = () => {
+  return (
+    <div className={styles.titleContainer}>
+       <HeaderName />
+       <div className={styles.toggle}>
+        <Toggle />
+      </div>
+    </div>
   )
 }
 
-export default Header
+export default MainHeader
